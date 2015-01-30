334fb1e767aa5a2740fd2deaa74374a691a6029b
Gianluca Interino <gianluca.interino@gmail.com>
2015-01-30 17:14:15 +0100

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testWebsiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testWebsiteApp
+ */
+angular.module('testWebsiteApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
